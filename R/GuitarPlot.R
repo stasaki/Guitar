cb413d533c0f1d3201c26a4e02c095df3ed0b30a
stasaki <stasaki@gmail.com>
2021-02-04 16:26:42 -0600

    Update GuitarPlot.R

--- R/GuitarPlot.R
+++ R/GuitarPlot.R
@@ -127,7 +127,7 @@ for (GroupName in names(sitesPointsRelative)) {
   sitesWeight <- sitesPointWeight[[GroupName]] / sum(sitesPointWeight[[GroupName]])
   siteID <- sitesPointsRelative[[GroupName]]
   # fit1 <- suppressWarnings(density(siteID, adjust = adjust,n=256,from=0,to=1, weight=sitesWeight))
-  fit1 <- suppressWarnings(density(siteID, adjust = adjust,from = 0, to = 1,n = 256, weight=sitesWeight))
+  fit1 <- suppressWarnings(density(siteID, adjust = adjust,from = 0, to = 1,n = 256, weight=sitesWeight,na.rm=T))
 
   
   tmp <-  data.frame(
